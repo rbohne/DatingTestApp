fbe28011006b9c7463b1bd5368a558c7dbf2a638
rbohne <rbohne@gmail.com>
2019-02-18 09:40:28 -0700

    End of Section 10

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  user: User;
+  // this is used so people can't close the browser without getting message about unsaved changes.
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService,
+              private userService: UserService, private authService: AuthService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data.user;
+    });
+  }
+
+  updateUser() {
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+
+    }, error => {
+      this.alertify.error(error);
+    });
+    this.alertify.success('Profile Updated Successfully');
+    this.editForm.reset(this.user);
+
+  }
+
+}
--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -39,6 +41,30 @@ namespace DatingApp.API.Controllers
 
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto) 
+        {
+            //check if the user's token matches the id they are passing in to update the data for
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+                
+            
+            var userFromRepo = await _repo.GetUser(id);
+
+            // this updates the values from userForUpdateDto to the userFromRepo 
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+                
+            throw new Exception($"Updating user {id} failed on save");
+
         }
 
     }
